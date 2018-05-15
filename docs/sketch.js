d583f170a03338ec097adca7f81f00a4ee0c5473
mommocmoc <mommocmoc@gmail.com>
2018-05-15 14:31:59 +0900

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -16,7 +16,7 @@ function setup() {
 function Shuffle(o) {
 	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
 	return o;
-};
+}
 
 function draw() {
 
